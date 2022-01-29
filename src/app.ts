6b2af055422b06d9c37736d3794d17b5a718ec3a
Łukasz Szpanelewski <lukasz@greensource.pl>
2022-01-29 22:54:04 +0100

    refactor: Change port env name to PORT

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json())
 app.use(defaultRouter)
 
-const PORT = process.env.APP_PORT ?? 3000
+const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
